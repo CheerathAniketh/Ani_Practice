ebbffd42b819c612c21684963e83218310eca8a8
CheerathAniketh <168458598+CheerathAniketh@users.noreply.github.com>
2025-09-18 07:18:02 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,10 @@
+// adding two no.s
+#include <stdio.h>
+
+int main(void)
+{
+    int a,b;
+    printf("Enter the values of a and b\n");
+    scanf("%i%i", &a,&b);
+    printf("SUM = %i\n", a+b);
+}
